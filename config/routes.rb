c02bad4d549911edd52ab07ee07bc3cd1e7a7f5c
kim parfait <kimtalya7@gmail.com>
2016-09-28 15:04:56 +0300

    friendship model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   
-  devise_for :users, :controller => { :registrations => "user/registrations" }
+  devise_for :users
   resources :user_stocks,except: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   get'my_portfolio', to:  "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
+  get 'my_friends' ,  to:  "users#my_friends"
 
 
 end
